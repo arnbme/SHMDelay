c5c5d357a0593d2854a41d7dab3f1c7131512b32
Arn Burkhoff <arnb@arnb.org>
2018-04-04 11:32:27 -0400

    Fix issue with burned pin move all documentation to community forum release thread change global keypad selection from capability to device

--- smartapps/arnbme/shm-delay.src/shm-delay.groovy
+++ smartapps/arnbme/shm-delay.src/shm-delay.groovy
@@ -14,6 +14,8 @@
  *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
  *  for the specific language governing permissions and limitations under the License.
  * 
+ *  Apr 04, 2018 v2.0.1  Fix issue with burned pin, move all documentation to community forum release thread,
+ *							change global keypad selection from capability to device
  *  Mar 20, 2018 v2.0.0  add reverse mode fix, user defined modes, set Alarm State when mode changes 
  *  Mar 18, 2018 v2.0.0  add Panic option
  *	Mar 14, 2018 v2.0.0  add logic that executes a Routine for a pin 
@@ -50,7 +52,7 @@ definition(
     name: "SHM Delay",
     namespace: "arnbme",
     author: "Arn Burkhoff",
-    description: "(V2.0.0)Smart Home Monitor Exit/Entry Delays with optional Keypad support",
+    description: "(V2.0.1)Smart Home Monitor Exit/Entry Delays with optional Keypad support",
     category: "My Apps",
     iconUrl: "https://www.arnb.org/IMAGES/hourglass.png",
     iconX2Url: "https://www.arnb.org/IMAGES/hourglass@2x.png",
@@ -59,10 +61,6 @@ definition(
 
 preferences {
     page(name: "main")
-    page(name: "aboutPage", nextPage: "installPage")
-    page(name: "installPage", nextPage: "delayPage")
-    page(name: "delayPage", nextPage: "createSimPage")
-    page(name: "createSimPage", nextPage: "main")
     page(name: "globalsPage", nextPage: "main")	
 }
 
@@ -119,7 +117,7 @@ def main()
 			{
 			section 
 				{
-				paragraph "Please read the documentation, review and set global settings, then complete the install by clicking 'Done' above before creating child profiles"
+				paragraph "Please read the documentation, review and set global settings, then complete the install by clicking 'Save' above. After the install completes, you may set Delay, User and Modefix profiles"
 				}
 			section
     			{
@@ -128,35 +126,13 @@ def main()
 			}	
 		section
 			{
-			href(name: "href",
-			title: "Introduction",
-			required: false,
-			page: "aboutPage")
+			href (url: "https://community.smartthings.com/t/beta-shm-delay-version-2-0/121800",
+			title: "Smartapp Documentation",
+			style: "external")
 			}
 		section
 			{
-			href(name: "href",
-			title: "Installation and Prerequisites",
-			required: false,
-			page: "installPage")
-			}
-		section
-			{
-			href(name: "href",
-			title: "How to create a Delay Profile",
-			required: false,
-			page: "delayPage")
-			}
-		section
-			{
-			href(name: "href",
-			title: "How to create a simulated contact sensor",
-			required: false,
-			page: "createSimPage")
-			}
-		section
-			{
-			paragraph "SHM Delay Version 2.0.0 Beta"
+			paragraph "SHM Delay Version 2.0.1 Beta"
 			}
 		remove("Uninstall SHM Delay","Warning!!","This will remove the ENTIRE SmartApp, including all profiles and settings.")
 		}
@@ -190,7 +166,7 @@ def globalsPage()
 				{
 				def actions = location.helloHome?.getPhrases()*.label
 				actions?.sort()
-				input "globalKeypadDevices", "capability.button", required: true, multiple: true,
+				input "globalKeypadDevices", "device.CentraliteKeypad", required: true, multiple: true,
 					title: "One or more Keypads used to arm and disarm SHM"
 				input "globalKeypadExitDelay", "number", required: true, range: "0..90", defaultValue: 30,
 					title: "True exit delay in seconds when arming in Away mode from any keypad. range 0-90, default:30"
@@ -216,126 +192,6 @@ def globalsPage()
 			}
 		}
 	}	
-def aboutPage()
-	{
-	dynamicPage(name: "aboutPage", title: "Introduction")
-		{
-		section 
-			{
-			paragraph "This smartapp simulates the Entry and Exit Delay parameters currently missing in SmartHome, "+
-			"giving you some time on entry to disarm the system using any method you choose prior to triggering an intrusion alert, "+
-			"and does not trigger an intrusion when a monitored contact sensor opens during the exit delay time. ***"+
-			"The delay operates only on monitored sensors, not the entire SmartHome application.***\n\n"+
-			"It also replaces the 'Open contact when system is armed' message, lost when removing the contact sensor from SmartHome.\n\n"+
- 			"***Please Note***: SmartHome is fully armed during operation of this SmartApp. Tripping a 'non-delayed sensor'"+ 
- 			" immediately triggers a SmartHome intrusion alert. This means a motion sensor that 'sees' a door"+
- 			" with a delayed sensor, or 'sees' a disarming device such as a keypad, will trigger an intrusion alert while you are disarming the system. The motion sensor, or disarming device must be relocated to successfully use this app." 
-			
-			}
-		}
-	}
-
-def installPage()
-	{
-	dynamicPage(name: "installPage", title: "Installation and Prerequisites")
-		{
-		section
-			{
-			paragraph "Installation:\n"+
-			"Please complete the install by clicking 'Done' on the main page before creating your first delay profile.\n\n"+
-			"The name of the simulated sensor appears in SmartHome intrusion messages, but many users prefer not"+ 
-			" to clutter their 'My Home' device list with simulated sensors. The default is non-unique (one) simulated sensor, the choice is yours. "+
-			"Should you prefer unique simulated sensors, expand the Global "+
-			"Application Settings, then set:\n"+
-			" 1. Simulated sensors must be unique? Default Off/False\n\n"+
-			" 2. Send intrusion notification with name of triggering real sensor?  Default On/True\n\n"+
-			" 3. Set True EntryDelay: A last resort when intusions cannot be stopped by setting an optional followed motion sensor in the Delay Profile. Default: Off/False\n\n"+
-			" 4. Set TrueNight flag on to trigger instant intrusion in Stay alarm state. Default: Off/False\n\n"+
-			" 5. Set mode flag if you want Mode to be synced with AlarmState Default: Off/False\n"+
-			" When set on: install app then create the ModeFix profile\n\n"+
-			" 6. When upgraded Keypad module is installed this must be set on. Default: Off/False\n\n"+
-			" 7. Select a 3 digit emergency number for inclusion on intrusion message (Optional)\n\n"+			
-			" 8. Add emergency phone number for inclusion on intrusion message (Optional)\n"+
-			"  Enter telephone number. Separate multiple numbers with a semicolon(;)\n\n"+
-			"Prerequisites:\n"+
-			" 1. When using non-unique simulated sensors: create one simulated contact sensor in the IDE.\n"+
-			"    When using unique simulated contact sensors: Create a simulated contact sensor for each monitored real sensor in the IDE.\n"+
-			"The name of the simulated sensor device is used by SmartHome on intrusion alert messages.\n\n"+
-			" 2. Remove the real contact sensor from SmartHome security monitoring. "+
-			"This app monitors this device and includes a replacement for the SmartHome 'system armed with open contact' monitor.\n\n"+
-			" 3. Add each simulated contact sensor to SmartHome's security monitoring. "+
-			"This app 'opens' the paired simulated sensor device, creating a SmartHome intrusion alert."
-			}
-		}
-	}
-
-def delayPage()
-	{
-	dynamicPage(name: "delayPage", title: "Creating a Delay Profile")
-		{
-		section
-			{
-			paragraph "Define a profile for each real contact sensor removed from SmartHome's monitoring, "+
-			"pairing it with a simulated contact sensor.\n"+ 
-			"Start by tapping 'Create A New Delay Profile'\n"+
-			"  1. select a real contact sensor\n"+
-			"  2. select a simulated contact device\n"+
-			"  3. (Optional) when a motion sensor triggers an intrusion during an entry delay: select motion sensor device, "+
-			"then remove it from SmartHome Armed (Away) Motion sensors\n"+
-			"  4. Profile name is internally set to Profile: (real contact sensor name) It may be modified\n"+
-			"  5. Tap 'Next' on top of page\n\n"+
-			"The 'Entry and Exit Data' page displays\n"+
-			"  1. set the entry delay time in seconds from 0 to 60. Default:30\n"+
-			" (zero is allowed enabling this app to be used only as an exit delay)\n"+
-			"  2. For away mode set the exit delay time in seconds from 0 to 60. Default:30\n"+
-			" (When using the Keypad exit delay in E Thayer's lock-manager, or any other exit delay method, set exit delay to 0)\n"+
-			" (This app's exit delay is not active on Stay or Night mode)\n"+
-			"  3. (Optional) set keypads where to sound the entrydelay tones\n"+
-			"  4. (Optional) set sirens where beep should be issued\n"+
-			"  When a siren does not support the beep command, the app issues the On command, followed in 250 millseconds by an Off command.\n"+ 
-			"  5. Tap 'Next' on top of page\n\n"+
-			"The 'Open Door Monitor and Notifications' page displays\n"+
-			"  Open Door Message Settings\n"+ 
-			"  1. set the maximum number of open door warning messages. Default: 2, Minimum: 1\n"+
-			"  2. set the number of minutes between open door messages from 1 to 15. Default: 1\n\n"+
-			"  Notifcation Settings for Open Door and optional Intrusion Alert messages\n"+ 	
-			"  3. should the message show in the notifications log. Default: true\n"+
-			"  4. should the message be issued as an application push notification. Default: true\n"+
-			"  5. should the message be sent by text (SMS) message. Default: false\n"+
-			"  Enter telephone number. Separate multiple numbers with a semicolon(;)\n"+
-			"  6. Tap 'Done' on top of page\n\n"
-			}
-		}
-	}
-
-def createSimPage()
-	{
-	dynamicPage(name: "createSimPage", title: "How to Create A Simulated Contact Sensor")
-		{
-		section 
-			{
-			href title: "1. Go to graph.api.smartthings.com", style: "external", 
-				url: "https://graph.api.smartthings.com"
-			paragraph " 2. Login\n"+
-			" 3. Click 'My Locations'\n"+
-			" 4. Under 'Name' click on the named location\n"+
-			" (The URL changes to the server name servicing this location. Smarthings calls this a shard)\n"+
-			" 5. On top menu: Click 'My Devices'\n"+ 
-			" 6. Click the 'New Device' button\n"+
-			" 7. Name: this can be whatever you want. Suggest adding 'SIM ' to the beginning of the real device's name. For example: SIM Front Door Sensor.\n"+ 
-			" 8. Label: this is optional and can be whatever you want.\n"+ 
-			" 9. Device Network Id: This can be anything you want, but it cannot duplicate other device ID's. For example SIM01.\n"+
-			"10. Zigbee Id: leave blank\n"+ 
-			"11. Type: from the dropdown select 'Simulated Contact Sensor'\n"+
-			"12. Version: select 'Published'\n"+
-			"13. Location: from the dropdown select your location name\n"+ 
-			"14. Hub: from the dropdown select your hub name.\n"+ 
-			"15. Group: you won't be able to select when creating, but these are Groups you may have created in the Things page in the SmartThings app.\n"+ 
-			"16. Click Create" 
-			}
-		}
-	}
-
 
 def installed() {
     log.debug "Installed with settings: ${settings}"
@@ -359,40 +215,7 @@ def initialize()
 		    subscribe (globalKeypadDevices, "contact.open", keypadPanicHandler)
 		    }
 		}
-/*	def armwk='away'		//this is all stuff used for testing
-	def kMap = [mode: armwk, dtim: now()]
-	atomicState.kMap=kMap					//save time any keypad armed/disarmed the system
-	def kMapin=atomicState.kMap
-	def kSecs = Math.round(kMapin.dtim / 1000)
-	log.debug "Keypad last mode test ${kMap} ${kMapin} ${kSecs} ${kMapin.mode} $kMapin.dtim}"
-	def wdlc="off"
-	log.debug "${wdlc.capitalize()}"
-	def theChild = findChildAppByName("SHM Delay ModeFix")
-	log.debug "offdefault is ${theChild.offDefault}"
-	log.debug "${location}"
-	log.debug "${location.helloHome}"
-	def childApps = getChildApps()		//gets all completed child apps
-	log.debug "there are ${childApps.size()} child smartapps"
-	childApps.each {
-	  	log.debug "child app: ${it.getLabel()} ${it.getName()} ${it.getId()}"
-
-		if (it.getName()=="SHM Delay User")	
-			{
-	  		log.debug "User: ${it.theuserpin} ${it.theusername} **${it.themaxcycles}**"
-			if (it.themaxcycles > 0)					//check if pin is burned
-				{
-				def atomicUseId=it.getId()+'uses'
-				log.debug "${atomicUseId}"
-				if (atomicState."${atomicUseId}" < 0)		//initialize if never set
-					atomicState."${atomicUseId}" = 3
-				def uses=atomicState."${atomicUseId}"
-				log.debug "uses ${uses}"
-				if (atomicState."${atomicUseId}" == 0)
-					log.debug "it is zero"
-				}	
-			}
-		}
-*/	}	
+	}	
 
 //  --------------------------Keypad support added Mar 02, 2018 V2-------------------------------
 /*				Basic location modes are Home, Night, Away. This can be very confusing
@@ -451,15 +274,15 @@ def keypadCodeHandler(evt)
 		    		{atomicState."${atomicUseId}" = atomicState."${atomicUseId}" + 1}
 		    	if (atomicState."${atomicUseId}" > it.themaxcycles)
 		    		{
+					log.debug "pin $codeEntered burned"
 					error_message = keypad.displayName + " Burned pin entered for " + it.theusername
-					return false		// end the **find** loop
 	    			}
 	    		}	
 			badPin=false
 //			log.debug "matched pin ${it.theuserpin} $it.pinScheduled"
 
 //			When pin is scheduled verify Dates, Weekday and Time Range	
-			if (it.pinScheduled)
+			if (error_message=="" && it.pinScheduled)
 				{
 //				keep this code in sync with similar code in SHM Delay Users				
     			def df = new java.text.SimpleDateFormat("EEEE")	//formatter for current time    			
@@ -568,6 +391,7 @@ def keypadCodeHandler(evt)
 
 	if (error_message!="")									// put out any messages to notification log
 		{
+//		log.debug error_message
 		sendNotificationEvent(error_message)
 		}
 		
@@ -611,7 +435,7 @@ def keypadCodeHandler(evt)
 		
 	keypad.acknowledgeArmRequest(modeEntered) 		//keypad demands a followup light setting or all lights blink
 	unschedule(execRoutine)		//Attempt to handle rearming/disarming during exit delay by unscheduling any pending away tasks 
-	atomicState.badpins=0		//reset badpin count
+//	atomicState.badpins=0		//reset badpin count
 	def armModes=['Home','Stay','Night','Away']
 	def message = keypad.displayName + " set mode to " + armModes[modeEntered] + " with pin for " + userName
 	def aMap = [data: [codeEntered: codeEntered, armMode: armModes[modeEntered]]]
